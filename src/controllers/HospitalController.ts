a91e76458777923f81cd55ff6f185a679409d2e1
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2020-04-28 14:55:14 +0200

    get hospital near location 1km  par default

--- src/controllers/HospitalController.ts
+++ src/controllers/HospitalController.ts
@@ -38,4 +38,45 @@ export default class HospitalController {
             return response.json(err).status(400);
         });
     };
+
+    /**
+     * @swagger
+     * /hospital/location:
+     *   post:
+     *     summary: Get All hospital near location
+     *     description: Returns a list of hospital near location
+     *     tags:
+     *       - Hospital
+     *     parameters:
+     *      - in body:
+     *        name: latitude
+     *        example: 2.369794931389816
+     *        type: string
+     *      - in body:
+     *        name: longitude
+     *        example: 48.7741237334045
+     *        type: string
+     *     responses:
+     *       200:
+     *         description: List of Hospital
+     *         schema:
+     *           type: object
+     *           properties:
+     *             hospital:
+     *               type: array
+     *               description: all the Hospital
+     *               items:
+     *                 type: string
+     */
+    static getHospitalByGeoCode = async (
+        request: Request,
+        response: Response,
+    ): Promise<Response> => {
+        const { latitude, longitude } = request.body;
+        return axios.get(`${process.env.API_HOSPITAL_URL as string}&geofilter.distance=${longitude}%2C+${latitude}%2C+1000`).then(result => {
+            return response.json(result.data).status(200);
+        }).catch(err => {
+            return response.json(err).status(400);
+        });
+    };
 }
--- src/routes/HospitalRoutes.ts
+++ src/routes/HospitalRoutes.ts
@@ -4,6 +4,7 @@ import { HospitalController } from '../controllers';
 const api = Router();
 
 api.get('/', HospitalController.getAllHospital);
+api.post('/location', HospitalController.getHospitalByGeoCode);
 
 
 export default api;
